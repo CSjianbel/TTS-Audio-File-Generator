98a915121cef7e2b5e3925412ac4af2e3ce6c944
OjouNii <belarminojiankarlo@gmail.com>
2022-08-30 00:07:54 +0800

    1 mp3 for each text file

--- tts_afg.py
+++ tts_afg.py
@@ -3,27 +3,49 @@ from gtts import gTTS
 import os
 
 # Constants
-AUDIO_DIR = "audio"
-TEXTS_FILENAME = "assets.txt"
+# AUDIO_DIR = "audio"
+# TEXTS_FILENAME = "assets.txt"
+TEXTS_DIR = "text"
+AUDIO_DIR = "audio_texts"
 LANG = "en"
-
 
 # Main function
 def main(): 
-  try:
+
+  if not os.path.exists(AUDIO_DIR):
     os.mkdir(AUDIO_DIR)
-  except OSError:
-    pass
 
-  with open(TEXTS_FILENAME, "r") as file:
-    words = set(file.readlines())
-    # print(words)
+  text_files = os.listdir(TEXTS_DIR)
 
-    for word in words:
-      word = word.strip()
-      print(f"Generating {word}.mp3...")
-      audio = gTTS(text=word, lang=LANG, slow=False)
-      audio.save(f"{AUDIO_DIR}/{word}.mp3")
+  for file in text_files:
+    # print(file)
+    with open(f"{TEXTS_DIR}/{file}", "r") as fp:
+      text = fp.read()
+      print(f"Generating {file}.mp3...")
+      audio = gTTS(text=text, lang=LANG, slow=True)
+      audio.save(f"{AUDIO_DIR}/{file}.mp3")
+
+
+  # try:
+  #   os.mkdir(AUDIO_DIR)
+  # except OSError:
+  #   pass
+
+  # with open(TEXTS_FILENAME, "r") as file:
+  #   words = set(file.readlines())
+  #   # print(words)
+
+  #   for word in words:
+  #     word = word.strip()
+  #     print(f"Generating {word}.mp3...")
+  #     audio = gTTS(text=word, lang=LANG, slow=False)
+  #     audio.save(f"{AUDIO_DIR}/{word}.mp3")
+
+RichText(
+  TextSpan(
+    text: ''
+  )
+)
 
 
 if __name__ == "__main__":
