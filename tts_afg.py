1d3d105aac3586b4b5e3388f5b60800c455bc6b5
OjouNii <belarminojiankarlo@gmail.com>
2022-08-30 22:35:25 +0800

    changed some stuff

--- tts_afg.py
+++ tts_afg.py
@@ -1,3 +1,4 @@
+from tkinter.tix import TEXT
 from gtts import gTTS
 
 import os
@@ -5,9 +6,13 @@ import os
 # Constants
 # AUDIO_DIR = "audio"
 # TEXTS_FILENAME = "assets.txt"
-TEXTS_DIR = "text"
-AUDIO_DIR = "audio_texts"
+# TEXTS_DIR = "text"
+# AUDIO_DIR = "audio_texts"
+# LANG = "en"
+
+AUDIO_DIR = "audio2"
 LANG = "en"
+TEXTS_FILENAME = "instructions.txt"
 
 # Main function
 def main(): 
@@ -15,15 +20,25 @@ def main():
   if not os.path.exists(AUDIO_DIR):
     os.mkdir(AUDIO_DIR)
 
-  text_files = os.listdir(TEXTS_DIR)
+  with open(TEXTS_FILENAME, "r") as fp:
+    text = fp.readlines()
+    for line in text:
+      line = line.split(' ', 1)
+      filename = line[0]
+      print(f"Generating {AUDIO_DIR}/{filename}.mp3...")
+      audio = gTTS(text=line[1], lang=LANG, slow=True)
+      audio.save(f"{AUDIO_DIR}/{filename}.mp3")
 
-  for file in text_files:
-    # print(file)
-    with open(f"{TEXTS_DIR}/{file}", "r") as fp:
-      text = fp.read()
-      print(f"Generating {file}.mp3...")
-      audio = gTTS(text=text, lang=LANG, slow=True)
-      audio.save(f"{AUDIO_DIR}/{file}.mp3")
+
+  # text_files = os.listdir(TEXTS_DIR)
+
+  # for file in text_files:
+  #   # print(file)
+  #   with open(f"{TEXTS_DIR}/{file}", "r") as fp:
+  #     text = fp.read()
+  #     print(f"Generating {file}.mp3...")
+  #     audio = gTTS(text=text, lang=LANG, slow=True)
+  #     audio.save(f"{AUDIO_DIR}/{file}.mp3")
 
 
   # try:
@@ -40,12 +55,6 @@ def main():
   #     print(f"Generating {word}.mp3...")
   #     audio = gTTS(text=word, lang=LANG, slow=False)
   #     audio.save(f"{AUDIO_DIR}/{word}.mp3")
-
-RichText(
-  TextSpan(
-    text: ''
-  )
-)
 
 
 if __name__ == "__main__":
